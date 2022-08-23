24e5200e7587a30c7a7623d3251124581ccae3b4
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-23 17:53:19 +0530

    ADDED: Compute a % b + c

--- arithmaticAndSorting.sh
+++ arithmaticAndSorting.sh
@@ -13,12 +13,11 @@ read c
 declare -i c=$c
 
 result[0]=$(($a+$b*$c))
-
 result[1]=$(($a*$b+$c))
-
 result[2]=$(($c+$a/$b))
+result[3]=$(($a%$b+$c))
 
 echo "${result[0]}"
 echo "${result[1]}"
 echo "${result[2]}"
-
+echo "${result[3]}"
